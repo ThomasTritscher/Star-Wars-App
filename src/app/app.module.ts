9040886c3c69ee619b6f9b820c8c285befcc04f8
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-11-07 11:00:27 +0100

    add main-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { MoviesComponent } from './movies/movies.component';
 import { CharactersComponent } from './characters/characters.component';
 import { PlanetsComponent } from './planets/planets.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     CharactersComponent,
-    PlanetsComponent
+    PlanetsComponent,
+    MainNavComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
